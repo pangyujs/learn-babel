5cc5d7e02108bd05554c75c6954389bcac0950c9
mengxiangyu <18322276773@163.com>
2023-03-18 16:54:12 +0800

    feat: babel加入换行的console.log

--- demo.js
+++ demo.js
@@ -2,6 +2,7 @@ const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 const generate = require("@babel/generator").default;
 const types = require("@babel/types");
+const template = require("@babel/template").default;
 
 const sourceCode = `
     console.log(1);
@@ -31,12 +32,22 @@ const targetCalleeName = ["log", "info", "error", "debug"].map(
 
 traverse(ast, {
   CallExpression(path, state) {
-    const calleeName = path.get("callee").toString();
+    if (path.node.isNew) return;
+    const calleeName = generate(path.node.callee).code;
     if (targetCalleeName.includes(calleeName)) {
       const { line, column } = path.node.loc.start;
-      path.node.arguments.unshift(
-        types.stringLiteral(`filename: (line:${line}, column:${column})`)
-      );
+
+      const newNode = template.expression(
+        `console.log("filename: (${line}, ${column})")`
+      )();
+      newNode.isNew = true;
+
+      if (path.findParent((path) => path.isJSXElement())) {
+        path.replaceWith(types.arrayExpression([newNode, path.node]));
+        path.skip();
+      } else {
+        path.insertBefore(newNode);
+      }
     }
   },
 });
