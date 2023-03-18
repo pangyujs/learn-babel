ecfafd6dbb424f78fd536b32534225c583e78811
mengxiangyu <18322276773@163.com>
2023-03-18 17:15:43 +0800

    feat: 变量命名规范化,移动文件规范化

--- runBabel.js
+++ runBabel.js
@@ -1,11 +1,11 @@
 const { transformFileSync } = require("@babel/core");
-const insertParamerersPlugin = require("./plugins/add-line-column-info");
+const insertParamsPlugin = require("./plugins/add-line-column-info");
 const path = require("path");
 
 const { code } = transformFileSync(
   path.join(__dirname, "./src/helloworld.js"),
   {
-    plugins: [insertParamerersPlugin],
+    plugins: [insertParamsPlugin],
     parserOpts: {
       sourceType: "unambiguous",
       plugins: ["jsx"],
